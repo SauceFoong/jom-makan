5e05a49e12c2f502b67709ba7e8e25472c8f2706
saucefoong <sheeyao@hotmail.com>
2021-09-12 21:47:06 +0800

    Created JomButton Component and Fixed Nav Bar NavLink route error

--- components/Footer.js
+++ components/Footer.js
@@ -12,31 +12,31 @@ import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 import { ReactNode } from "react";
 import NextLink from "next/link";
 
-const SocialButton = ({ children, label, href }) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
-      rounded={"full"}
-      w={8}
-      h={8}
-      cursor={"pointer"}
-      as={"a"}
-      href={href}
-      display={"inline-flex"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      transition={"background 0.3s ease"}
-      _hover={{
-        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  );
-};
+// const SocialButton = ({ children, label, href }) => {
+//   return (
+//     <chakra.button
+//       bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+//       rounded={"full"}
+//       w={8}
+//       h={8}
+//       cursor={"pointer"}
+//       as={"a"}
+//       href={href}
+//       display={"inline-flex"}
+//       alignItems={"center"}
+//       justifyContent={"center"}
+//       transition={"background 0.3s ease"}
+//       _hover={{
+//         bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+//       }}
+//     >
+//       <VisuallyHidden>{label}</VisuallyHidden>
+//       {children}
+//     </chakra.button>
+//   );
+// };
 
-export default function SmallWithSocial() {
+export default function Footer() {
   return (
     <Box
       bg={useColorModeValue("gray.50", "gray.900")}
