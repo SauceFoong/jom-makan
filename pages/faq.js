c26dfc19a1803e79f754bacdd1bcaeb4e571a838
saucefoong <sheeyao@hotmail.com>
2021-10-10 22:24:35 +0800

    Fixed quotation symbol error

--- pages/faq.js
+++ pages/faq.js
@@ -25,7 +25,9 @@ const Faq = () => {
       <Accordion allowMultiple>
         <AccordionItem>
           <AccordionButton fontSize={25}>
-            <Box paddingTop= {2} paddingLeft = {5} paddingBottom = {2}>1. How to create an Order?</Box>
+            <Box paddingTop={2} paddingLeft={5} paddingBottom={2}>
+              1. How to create an Order?
+            </Box>
           </AccordionButton>
           <AccordionPanel>
             <Table variant={"simple"} border="hidden">
@@ -111,230 +113,240 @@ const Faq = () => {
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem>
-          {({isExpanded}) => (
+          {({ isExpanded }) => (
             <>
-         <AccordionButton fontSize={25}>
-          <Box paddingTop= {2} paddingLeft = {5} paddingBottom = {2}>2. How to join a Jom?</Box>
-         </AccordionButton>
-        <AccordionPanel>
-            <Table variant={"simple"} border="hidden">
-              <Tbody>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  Steps to join a Jom in Jom Makan:
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  a. Click on the Order &gt; Today Orders
-                  </Td>
-                </Tr>
-                <Tr border={"hidden"}>
-                  <Td>
-                    <Image src="../../jom-makan-faq-jom.png" />
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  b. Click on the Order that you're interested in to view more details about the Order.
-                  </Td>
-                </Tr>
-                <Tr border={"hidden"}>
-                  <Td>
-                    <Image src="../../jom-makan-faq-details.png" />
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  c. You can click on the &quot;Jom&quot; button to join the Jom.
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  d. After that, you can submit what you wanted to order 
-                  in the Remark section via the Jom.
-                  </Td>
-                </Tr>
-                <Tr border={"hidden"}>
-                  <Td>
-                    <Image src="../../jom-makan-faq-jom-test.png" />
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  e. You are only allowed to edit the Remark before 
-                  the order closing time and date.
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  f. You may also choose the payment method that you prefer in the Jom.
-                  </Td>
-                </Tr>
-                <Tr border={"hidden"}>
-                  <Td>
-                    <Image src="../../jom-makan-faq-payment.png" />
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  g. Once you done selecting the payment method and filled the order remark,
-                  you may click on the &quot;Confirm&quot; button to submit your Jom order.
-                  Your Jom order will be immediately reflected at the order&apos;s details.
-                  </Td>
-                </Tr>
-                <Tr border={"hidden"}>
-                  <Td>
-                    <Image src="../../jom-makan-faq-jom-detail.png"/>
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'} fontStyle = {'italic'}>
-                  <Td>
-                  Above are the steps to join a Jom in Jom Makan. Join a Jom now!
-                  </Td>
-                </Tr>
-              </Tbody>
-            </Table>
-        </AccordionPanel>
-        </>
+              <AccordionButton fontSize={25}>
+                <Box paddingTop={2} paddingLeft={5} paddingBottom={2}>
+                  2. How to join a Jom?
+                </Box>
+              </AccordionButton>
+              <AccordionPanel>
+                <Table variant={"simple"} border="hidden">
+                  <Tbody>
+                    <Tr border={"hidden"}>
+                      <Td>Steps to join a Jom in Jom Makan:</Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>a. Click on the Order &gt; Today Orders</Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        <Image src="../../jom-makan-faq-jom.png" />
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        b. Click on the Order that you&apos;re interested in to
+                        view more details about the Order.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        <Image src="../../jom-makan-faq-details.png" />
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        c. You can click on the &quot;Jom&quot; button to join
+                        the Jom.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        d. After that, you can submit what you wanted to order
+                        in the Remark section via the Jom.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        <Image src="../../jom-makan-faq-jom-test.png" />
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        e. You are only allowed to edit the Remark before the
+                        order closing time and date.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        f. You may also choose the payment method that you
+                        prefer in the Jom.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        <Image src="../../jom-makan-faq-payment.png" />
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        g. Once you done selecting the payment method and filled
+                        the order remark, you may click on the
+                        &quot;Confirm&quot; button to submit your Jom order.
+                        Your Jom order will be immediately reflected at the
+                        order&apos;s details.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        <Image src="../../jom-makan-faq-jom-detail.png" />
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"} fontStyle={"italic"}>
+                      <Td>
+                        Above are the steps to join a Jom in Jom Makan. Join a
+                        Jom now!
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </AccordionPanel>
+            </>
           )}
         </AccordionItem>
         <AccordionItem>
-          {({isExpanded}) => (
+          {({ isExpanded }) => (
             <>
-         <AccordionButton fontSize={25}>
-          <Box paddingTop= {2} paddingLeft = {5} paddingBottom = {2}>3. How to upload receipt after paying in Jom Makan?</Box>
-         </AccordionButton>
-         <AccordionPanel>
-            <Table variant={"simple"} border="hidden">
-              <Tbody>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  Steps to upload a receipt in Jom Makan:
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  a. Click on the Order &gt; Your Joms
-                  </Td>
-                </Tr>
-                <Tr border={"hidden"}>
-                  <Td>
-                    <Image src="../../jom-makan-faq-yourjoms.png" />
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  b. Click on the Jom that you've joined.
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  c. Move over to your Jom record in the Order Details and click on the &quot;Upload&quot; button. 
-                  </Td>
-                </Tr>
-                <Tr border={"hidden"}>
-                  <Td>
-                    <Image src="../../jom-makan-faq-jom-detail.png" />
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  d. You may upload your receipt in either PNG, JPEG and PDF format without exceeding 5MB and click the &quot;Save&quot; button to upload. 
-                  </Td>
-                </Tr>
-                <Tr border={"hidden"}>
-                  <Td>
-                    <Image src="../../jom-makan-faq-upload-receipt.png" />
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'} fontStyle = {'italic'}>
-                  <Td>
-                  Above are the steps to upload a receipt in Jom Makan. Join a Jom now!
-                  </Td>
-                </Tr>
-
-              </Tbody>
-            </Table>
-        </AccordionPanel>
-        </>
+              <AccordionButton fontSize={25}>
+                <Box paddingTop={2} paddingLeft={5} paddingBottom={2}>
+                  3. How to upload receipt after paying in Jom Makan?
+                </Box>
+              </AccordionButton>
+              <AccordionPanel>
+                <Table variant={"simple"} border="hidden">
+                  <Tbody>
+                    <Tr border={"hidden"}>
+                      <Td>Steps to upload a receipt in Jom Makan:</Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>a. Click on the Order &gt; Your Joms</Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        <Image src="../../jom-makan-faq-yourjoms.png" />
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>b. Click on the Jom that you&apos;ve joined.</Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        c. Move over to your Jom record in the Order Details and
+                        click on the &quot;Upload&quot; button.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        <Image src="../../jom-makan-faq-jom-detail.png" />
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        d. You may upload your receipt in either PNG, JPEG and
+                        PDF format without exceeding 5MB and click the
+                        &quot;Save&quot; button to upload.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        <Image src="../../jom-makan-faq-upload-receipt.png" />
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"} fontStyle={"italic"}>
+                      <Td>
+                        Above are the steps to upload a receipt in Jom Makan.
+                        Join a Jom now!
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </AccordionPanel>
+            </>
           )}
-         </AccordionItem>
-         <AccordionItem>
-          {({isExpanded}) => (
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
             <>
-         <AccordionButton fontSize={25}>
-          <Box paddingTop= {2} paddingLeft = {5} paddingBottom = {2}>4. Is there any way to view my previous Orders?</Box>
-         </AccordionButton>
-         <AccordionPanel>
-            <Table variant={"simple"} border="hidden">
-              <Tbody>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  Step to view previous Orders in Jom Makan:
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  a. Click on the Order &gt; Your Orders. 
-                  You will see all your previous Orders in the secton below it.
-                  </Td>
-                </Tr>
-                <Tr border = {'hidden'} fontStyle = {'italic'}>
-                  <Td>
-                  Above is the step to view previous Orders in Jom Makan. Create an Order at Jom Makan now!
-                  </Td>
-                </Tr>
-              </Tbody>
-            </Table>
-        </AccordionPanel>
-        </>
+              <AccordionButton fontSize={25}>
+                <Box paddingTop={2} paddingLeft={5} paddingBottom={2}>
+                  4. Is there any way to view my previous Orders?
+                </Box>
+              </AccordionButton>
+              <AccordionPanel>
+                <Table variant={"simple"} border="hidden">
+                  <Tbody>
+                    <Tr border={"hidden"}>
+                      <Td>Step to view previous Orders in Jom Makan:</Td>
+                    </Tr>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        a. Click on the Order &gt; Your Orders. You will see all
+                        your previous Orders in the secton below it.
+                      </Td>
+                    </Tr>
+                    <Tr border={"hidden"} fontStyle={"italic"}>
+                      <Td>
+                        Above is the step to view previous Orders in Jom Makan.
+                        Create an Order at Jom Makan now!
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </AccordionPanel>
+            </>
           )}
-         </AccordionItem>
-         <AccordionItem>
-          {({isExpanded}) => (
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
             <>
-         <AccordionButton fontSize={25}>
-          <Box paddingTop= {2} paddingLeft = {5} paddingBottom = {2}>5. Do I need to go with the Order&apos;s 
-          owner when I joined a Jom?</Box>
-         </AccordionButton>
-         <AccordionPanel>
-            <Table variant={"simple"} border="hidden">
-              <Tbody>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  You don't need to go with the Order&apos;s owner when you joined the Jom. 
-                  The owner will deliver the food to your table.
-                  </Td>
-                </Tr>
-              </Tbody>
-            </Table>
-        </AccordionPanel>
-        </>
+              <AccordionButton fontSize={25}>
+                <Box paddingTop={2} paddingLeft={5} paddingBottom={2}>
+                  5. Do I need to go with the Order&apos;s owner when I joined a
+                  Jom?
+                </Box>
+              </AccordionButton>
+              <AccordionPanel>
+                <Table variant={"simple"} border="hidden">
+                  <Tbody>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        You don&apos;t need to go with the Order&apos;s owner
+                        when you joined the Jom. The owner will deliver the food
+                        to your table.
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </AccordionPanel>
+            </>
           )}
-         </AccordionItem>
-         <AccordionItem>
-          {({isExpanded}) => (
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
             <>
-         <AccordionButton fontSize={25}>
-          <Box paddingTop= {2} paddingLeft = {5} paddingBottom = {2}>6. Why do I need to pay the tips to the Order&apos;s
-          owner when I joined a Jom?</Box>
-         </AccordionButton>
-         <AccordionPanel>
-            <Table variant={"simple"} border="hidden">
-              <Tbody>
-                <Tr border = {'hidden'}>
-                  <Td>
-                  The tips paid to the owner is the recognition to the owner&apos;s effort for 
-                  bringing the food from outside to your table. 
-                  </Td>
-                </Tr>
-              </Tbody>
-            </Table>
-        </AccordionPanel>
-        </>
+              <AccordionButton fontSize={25}>
+                <Box paddingTop={2} paddingLeft={5} paddingBottom={2}>
+                  6. Why do I need to pay the tips to the Order&apos;s owner
+                  when I joined a Jom?
+                </Box>
+              </AccordionButton>
+              <AccordionPanel>
+                <Table variant={"simple"} border="hidden">
+                  <Tbody>
+                    <Tr border={"hidden"}>
+                      <Td>
+                        The tips paid to the owner is the recognition to the
+                        owner&apos;s effort for bringing the food from outside
+                        to your table.
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </AccordionPanel>
+            </>
           )}
-         </AccordionItem>
+        </AccordionItem>
       </Accordion>
     </div>
   );
