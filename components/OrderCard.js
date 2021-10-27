5602642505696bed91754837b71f4dace8c17824
saucefoong <sheeyao@hotmail.com>
2021-10-28 06:16:32 +0800

    Fixed tags id duplicated error

--- components/OrderCard.js
+++ components/OrderCard.js
@@ -166,23 +166,21 @@ const OrderCard = ({
             as={AiOutlineTag}
             color={useColorModeValue("gray.900", "white")}
           />
-          {tags.length >= 1 ?
-            tags.map((tag, i) => (
-              <Tag
-                key={i}
-                size="md"
-                key="md"
-                variant="solid"
-                colorScheme="teal"
-                marginTop="1"
-                marginRight="1"
-                style={{ textTransform: "capitalize" }}
-              >
-                {tag}
-              </Tag>
-            ))
-            : '-'
-          }
+          {tags.length >= 1
+            ? tags.map((tag, i) => (
+                <Tag
+                  key={i}
+                  size="md"
+                  variant="solid"
+                  colorScheme="teal"
+                  marginTop="1"
+                  marginRight="1"
+                  style={{ textTransform: "capitalize" }}
+                >
+                  {tag}
+                </Tag>
+              ))
+            : "-"}
         </ListItem>
       </List>{" "}
       {yourOrder || yourJom ? null : (
